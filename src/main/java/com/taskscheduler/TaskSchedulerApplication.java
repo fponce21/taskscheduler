413f6b0688b1d3aeefe5697d48c67b69a59ac690
f_ponce21 <ponce21francisco@gmail.com>
2025-06-30 13:11:10 -0700

    main app run class added

---
+++ src/main/java/com/taskscheduler/TaskSchedulerApplication.java
@@ -0,0 +1,12 @@
+package com.taskscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main entry point for the Spring Boot application
+@SpringBootApplication
+public class TaskSchedulerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSchedulerApplication.class, args);
+	}
+}
